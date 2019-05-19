55da33864509644b6eb517399a7df44be224f960
Hiras <hirasharis03@gmail.com>
2019-05-19 13:34:24 +0530

    paf finish

--- paf/src/main/java/com/hiras/paf/SupplierAplication.java
+++ paf/src/main/java/com/hiras/paf/SupplierAplication.java
@@ -8,9 +8,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class SupplierAplication {
-	
+
 	public static void main(String[] args) {
-	SpringApplication.run(SupplierAplication.class, args);
-	
+		SpringApplication.run(SupplierAplication.class, args);
+		// TODO Auto-generated method stub
+
 	}
-	}
+
+}
